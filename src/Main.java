880c128d87702dacfe8419705195aaf4c0d0bd02
Nicole0509 <niashavril0509@gmail.com>
2025-09-09 13:37:00 +0200

    converting celcius to fahrenheit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        float celcius = (float) 21.7;
+
+        System.out.println("Temperature Converter");
+        System.out.println("Temperature in celcius: " + celcius +" C");
+
+        float fahrenheit =  (float)(celcius * 9/5) + 32;
+
+        System.out.println("Temperature in fahrenheit: " + fahrenheit +" F");
+
+
+    }
+}
