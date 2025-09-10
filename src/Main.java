fe3ef74419c3ff0094a71e80a172d52f35e04619
Nicole0509 <niashavril0509@gmail.com>
2025-09-10 14:10:14 +0200

    Added a few more temperatures

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,15 @@ public class Main {
 
         System.out.println("Temperature Converter\n");
 
-        double[] celsiusTemperatureValues = {25, 0, -40, 100, -300};
+        double[] celsiusTemperatureValues = {25, 0, -40, 100, -230, 83, 15, -300};
 
         for(double celsius : celsiusTemperatureValues){
 
             if(celsius < -273.15){
-                System.out.println("Attention!! " + celsius + " °C is below absolute zero...");
+                System.out.println("Attention!! " + celsius + " °C is below absolute zero...\n");
             } else {
                 double fahrenheit =  (celsius * 9/5) + 32;
-                System.out.println(celsius + " °C is " + fahrenheit +" °F");
+                System.out.println(celsius + " °C is " + fahrenheit +" °F\n");
             }
 
         }
