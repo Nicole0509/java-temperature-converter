a1d961b8ab82aa1ec9d83c5a01fef6e62c8e5985
Nicole0509 <niashavril0509@gmail.com>
2025-09-10 13:48:10 +0200

    Adding validation to check if the temperature is below zero

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        double celsius =  -273.15;
+        Scanner input = new Scanner(System.in);
 
         System.out.println("Temperature Converter");
+
+        System.out.print("Enter temperature in Celsius: ");
+
+        double celsius = input.nextDouble();
 
         double fahrenheit =  (celsius * 9/5) + 32;
 
