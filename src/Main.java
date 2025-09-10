33d86fc9f338b8da3d5a5ce16974623e6e6438d4
Nicole0509 <niashavril0509@gmail.com>
2025-09-10 13:56:57 +0200

    Added the absolute zero validation

--- src/Main.java
+++ src/Main.java
@@ -13,14 +13,12 @@ public class Main {
 
         double celsius = input.nextDouble();
 
-        double fahrenheit =  (celsius * 9/5) + 32;
-
-        if(celsius < 0){
-            System.out.println("Attention!! The temperature is below zero...");
+        if(celsius < -273.15){
+            System.out.println("Attention!! " + celsius + " °C is below absolute zero...");
+        } else {
+            double fahrenheit =  (celsius * 9/5) + 32;
+            System.out.println(celsius + " °C is " + fahrenheit +" °F");
         }
-
-        System.out.println(celsius + " °C is " + fahrenheit +" °F");
-
 
     }
 }
